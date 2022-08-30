9fb29810dba7a3bfb48e3e76fb0e72affcd40df7
Louis <louis.rds@akticube.fr>
2022-08-30 19:05:21 +0200

    Ajout __init__.py

---
+++ azauthpy/__init__.py
@@ -0,0 +1,14 @@
+
+"""
+AzAuthPY
+~~~~~~~~~~~~~~~~~~~
+A python library for your Azuriom authentication  API
+:copyright: (c) 2020-present AktiCube
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'AzAuthPY'
+__author__ = 'AktiCube'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020-present AktiCube'
+__version__ = '1.0.0'
